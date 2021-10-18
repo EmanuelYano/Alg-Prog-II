cc4328bd72691fd0f5bd0b269b97552c177aec3a
Emanuel <yano.emanuel.21@gmail.com>
2021-10-17 23:10:41 -0400

    T1 finalizado

--- T1/emanuel_yano.c
+++ T1/emanuel_yano.c
@@ -1,9 +1,18 @@
+/***************************************************
+ * *Emanuel Ricardo Yano
+ * *Trabalho 1
+ * *Professor(a): Diego Rubert
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct{
         int posicao_cap;
         int qtd_tp;
+        int aux;
 }capivara;
 
 void troca(int *x, int *y){
@@ -14,76 +23,61 @@ void troca(int *x, int *y){
 }
 
 int separa(int p, int r, capivara capi[]){
-    int x,i,j;
+    int  i,j,x,y;
+    y = capi[p].aux;
     x = capi[p].qtd_tp;
     i = p - 1;
     j = r + 1;
-    while(i<j){
-        do{
+    while (i < j){
+        do {
             j--;
-        }while(capi[j].qtd_tp > x);
+        } while (capi[j].qtd_tp < x || (capi[j].qtd_tp == x && capi[j].aux > y));
         do{
-            i++;;
-        }while(capi[i].qtd_tp < x);
-        if(i<j){                        
-            troca(&capi[i].qtd_tp,&capi[j].qtd_tp);
-            troca(&capi[i].posicao_cap,&capi[j].posicao_cap);           
-        }else    
-            return j;
+            i++;
+        } while (capi[i].qtd_tp > x || (capi[i].qtd_tp == x && capi[i].aux < y));
+        if (i < j){
+            troca(&capi[i].aux, &capi[j].aux);
+            troca(&capi[i].qtd_tp, &capi[j].qtd_tp);
+        }
     }
+    return j;
 }
 
 void orde_rapida(int p, int r, capivara capi[]){
     int q;
-    //for(int i=0; i<r;i++)    
-    //    printf("%d",capi[i].qtd_tp);
     if(p < r){
         q = separa(p,r,capi);
         orde_rapida(p,q,capi);
         orde_rapida(q+1,r,capi);
     }
+
 }
 
-void padawan(capivara capi[], int n){
-    for(int i=0; i<n; i++){
+void imprimi_vetores(capivara capi[], int n){
+    for(int i=0; i<n; i++)
         printf("%d ", capi[i].posicao_cap);
-        printf("%d ", capi[i].qtd_tp);
-        printf("\n");
-    }
-    
+    printf("\n");
+    for(int k=0; k<n; k++)
+        printf("%d ", capi[k].aux);
+    printf("\n");
 }
+
 int main(){
-    int num_cap,teleporte, num_rep=-1, fim_rep;
+    int num_cap,teleporte;
     scanf("%d",&num_cap);
     capivara capi[num_cap];
     for (int i = 0; i < num_cap; i++){
         capi[i].posicao_cap = i+1;
+        capi[i].aux = i+1;
         capi[i].qtd_tp = 0;
-    }  
-    while ( teleporte != 0) {
-        scanf("%d", &teleporte);
-        capi[teleporte-1].qtd_tp += 1; 
+    } 
+    while ( scanf("%d", &teleporte) != EOF) {
+        capi[teleporte-1].qtd_tp += 1;
+        for(int i=0; i<num_cap; i++)
+            if(teleporte == capi[i].posicao_cap && teleporte > 0)
+                troca(&capi[i].posicao_cap,&capi[i-1].posicao_cap);
     }
-    orde_rapida(0,num_cap,capi);
-    padawan(capi,num_cap);
-    for(int i=0; i<num_cap-2; i++){
-        int num_teste = capi[i].qtd_tp;
-        if(num_teste == capi[i+1].qtd_tp && num_rep == -1)
-            num_rep = i;
-        else if(num_rep == capi[i+1].qtd_tp)
-            fim_rep = i;        
-    }
-    printf("\n%d %d", num_rep, fim_rep);
-    //orde_rapida(0,num_cap,capi.posicao_cap);
-    
-    /*for (int i = 0; i < num_cap; i++){
-        printf("%d ",proximo[i].posicao_cap);
-        printf("%d ",proximo[i].qtd_tp);
-        printf("\n");
-    }*/
-        
-    
-
-    
+    orde_rapida(0,num_cap-1,capi);
+    imprimi_vetores(capi,num_cap);  
     return 0;
 }
